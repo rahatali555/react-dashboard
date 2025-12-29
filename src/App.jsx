7efcbf74792adb6b2e4a1fd18cd674da03cfc330
rahatali555 <ali.rahat555@gmail.com>
2025-12-30 00:06:05 +0500

    feat: add Avatar and UserInfo components

--- src/App.jsx
+++ src/App.jsx
@@ -9,3 +9,10 @@ function Avatar (){return ( <img src="https://via.placeholder.com/100"
 />
 );
 }
+function UserInfo() {
+  return ( <div>
+    <h1> Ali Haider </h1>
+    <h2> fRONT eND dEVELOPER</h2>
+    </div>
+  );
+}
