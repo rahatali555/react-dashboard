35e3301859908b97c6f6ed002c8fa63460ac1e3f
rahatali555 <ali.rahat555@gmail.com>
2026-03-10 00:05:53 +0500

    just learning

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+
+
 function MyButton(){
   return(
-    <button>Am Button</button>
+    <button>Hi There!</button>
   );
 }
 function App(){
-   const user={name:"rahat"};
+  const user=
+  {
+    name:"rahi",
+    age:22,
+    adDress:"lahore",
+   
+  }
   return(
     <>
-    <h1>{user.name}</h1>
-    <MyButton/>
+    <h1>{user.name}({user.age})({user.adDress})
+      </h1>
+      <MyButton/>
     </>
-   
-    
-   
-  );
+  )
 }
 export default App;
