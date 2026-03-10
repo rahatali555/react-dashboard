a897ab347268b8e4715fc488783de68ab2ffec05
rahatali555 <ali.rahat555@gmail.com>
2026-03-10 21:48:56 +0500

    finally added pic and got the perfect idea how to do it

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 
-
 function MyButton(){
   return(
-    <button>Hi There!</button>
-  );
+    <button> Hi there!</button>
+  )
+}
+function MyName(){
+  return(
+    <>
+    <h1> My Name </h1>
+    <p> i am software engineer </p>
+    <MyButton />
+    <img  src="/images/download.png" alt="react pic" width='200' height='200'/>
+    
+    </>
+  )
 }
 function App(){
-  const user=
-  {
+  const User= {
     name:"rahi",
-    age:22,
-    adDress:"lahore",
-   
+    age:32,
+    Addres:"Lahore"
   }
   return(
     <>
-    <h1>{user.name}({user.age})({user.adDress})
-      </h1>
-      <MyButton/>
+    <h1>{User.name} {User.age} {User.Address}</h1>
+  
+    <MyName/>
+
     </>
   )
 }
