54cebdac47ab63f98aa42ca4dfcdaf77b5dcfb40
rahatali555 <ali.rahat555@gmail.com>
2026-03-10 22:41:45 +0500

    practice conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,69 @@
 
-function MyButton(){
-  return(
-    <button> Hi there!</button>
-  )
-}
-function MyName(){
-  return(
-    <>
-    <h1> My Name </h1>
-    <p> i am software engineer </p>
-    <MyButton />
-    <img  src="/images/download.png" alt="react pic" width='200' height='200'/>
-    
-    </>
-  )
-}
-function App(){
-  const User= {
-    name:"rahi",
-    age:32,
-    Addres:"Lahore"
-  }
-  return(
-    <>
-    <h1>{User.name} {User.age} {User.Address}</h1>
-  
-    <MyName/>
+// function MyButton(){
+//   return(
+//     <button> Hi there!</button>
+//   )
+// }
+// function MyName(){
+//   return(
+//     <>
+//     <h1> My Name </h1>
+//     <p> i am software engineer </p>
+//     <MyButton />
+//     <img  src="/images/download.png" alt="react pic" width='200' height='200'/>
 
-    </>
+
+
+    
+//     </>
+//   )
+// }
+// function App(){
+//   const User= {
+//     name:"rahi",
+//     age:32,
+//     Addres:"Lahore"
+//   }
+//   return(
+//     <>
+//     <h1>{User.name} {User.age} {User.Address}</h1>
+  
+//     <MyName/>
+
+//     </>
+//   )
+// }
+// export default App;
+
+function AdminPanel(){
+  return(
+  
+<> 
+<h2>AdminPanel</h2>
+</>
+    
   )
 }
-export default App;
+ function UserPanel(){
+  return(
+    <>
+    <h2>userpanel</h2> </>
+  )
+ }
+ function App(){
+  let IsloggedIN=true;
+  let content;
+  if(IsloggedIN)
+{
+  content =<AdminPanel/>;
+} 
+else{
+  content=<UserPanel/>;
+}
+return(
+  <>
+  {content}
+  </>
+)
+ }
+ export default App;
