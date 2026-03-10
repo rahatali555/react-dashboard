84db5b5062aef565b2530cc1069775b93058948b
rahatali555 <ali.rahat555@gmail.com>
2026-03-10 23:24:03 +0500

    practicing again

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,29 @@
-
-// function MyButton(){
-//   return(
-//     <button> Hi there!</button>
-//   )
-// }
-// function MyName(){
-//   return(
-//     <>
-//     <h1> My Name </h1>
-//     <p> i am software engineer </p>
-//     <MyButton />
-//     <img  src="/images/download.png" alt="react pic" width='200' height='200'/>
-
-
-
+function MyButton(){
+  return(
+    <>
     
-//     </>
-//   )
-// }
-// function App(){
-//   const User= {
-//     name:"rahi",
-//     age:32,
-//     Addres:"Lahore"
-//   }
-//   return(
-//     <>
-//     <h1>{User.name} {User.age} {User.Address}</h1>
-  
-//     <MyName/>
-
-//     </>
-//   )
-// }
-// export default App;
-
-function AdminPanel(){
-  return(
-    <>
-    <h1> Admin Panel</h1>
-    </>     
-  );
+    <button> Click here!</button>
+  </>);
 }
-function UserPanel(){
-  return (
-    <>
-    <h2> User Panel </h2>
-    </>
-  );
-}
-
-function App (){
-  let isloggedIn=false
-  let content;
-  if(isloggedIn){
-    content=<AdminPanel/>
-  } 
-  else{
-    content=<UserPanel/>
+function MyName(){
+  const user={
+    name:"rahat",
+    age:32,
+    address:"lahore"
   }
+
   return(
     <>
-    "{content}
+    <h2> My name is {user.name}and my age is {user.age} and am living in {user.address}</h2>
+    
+    <MyButton/>
     </>
   )
 }
-export default App;
+export default function(){
+  return(
+    <>
+    <MyName/>
+    </>
+  )
+}
