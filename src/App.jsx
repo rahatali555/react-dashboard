c5b0ff65faa8dd50d1ba15e0afb71468a3e123ae
rahatali555 <ali.rahat555@gmail.com>
2026-03-12 00:06:49 +0500

    practicing with for loops

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ const products=[
   {title:'Garlic', isFruit:false, id:2},
   {title:'Apple', isFruit:false, id:3},
 ];
-function ShoppingList(){
-  const ListItems = products.map(products=>
-    <li
-    key={products.id}
-    style={{
-      color:products.isFruit ? 'magenta':'darkgreen'
-    }}
-    >
-      {products.title}
+ export default function ShoppingList(){
 
-      
+  const listItems=[];
+
+  for( let i=0;i < products.length;i++){
+    const product= products[i];
+    listItems.push(
+      <li 
+       key={product.id}
+       style={{
+         color: product.isFruit ? "magenta" : "darkgreen"
+       }}
+       >
+        {product.title}
 
 
-    </li>
+      </li>
+    )
 
+  }
+  return(
+    <ul> {listItems} </ul>
   )
 }
