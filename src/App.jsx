e3174c9babc64840774eeb4aa60d8c260d5ab53a
rahatali555 <ali.rahat555@gmail.com>
2026-03-11 23:01:09 +0500

    rendering list

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 const products=[
-  {title:'cabbage', isFruit:false,id:1},
-  {TITLE:'Garlic',isFruit:false, id:2},
-  {title:'Apple', isFruit:true, id:3},
+  {title:'cabbage', isFruit:false, id:1},
+  {title:'Garlic', isFruit:false, id:2},
+  {title:'Apple', isFruit:false, id:3},
 ];
- 
 function ShoppingList(){
-  const ListItems =products.map(products=>
-    <li 
+  const ListItems = products.map(products=>
+    <li
     key={products.id}
     style={{
-      color:products.isFruit ? 'magenta': 'darkgreen'
+      color:products.isFruit ? 'magenta':'darkgreen'
     }}
     >
       {products.title}
 
+      
+
+
     </li>
-  );
-  return (
-    <>
-    <h1> My Shopping List </h1>
-    <ul> {ListItems}  </ul>
-    </>
+
   )
 }
-export default ShoppingList;
