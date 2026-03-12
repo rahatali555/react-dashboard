c5973531df7a77454b839b997ce3964e11b80015
rahatali555 <ali.rahat555@gmail.com>
2026-03-12 15:21:03 +0500

    practice again

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-const products= [
-  { title:'cabbage', isFRUIT:false, id:1 },
-  { title:'garlic', isFRUIT:false, id:2},
-  { title:'Apple', isFRUIT:true, id:3 },
+const products=[
+  { title:'cabbage', isFruit:false, id:1},
+  { title:'carrot', isFruit:false, id:2},
+  { title:'Apple', isFruit:true, id:3},
 ];
-export default function ShoppingList(){
-  const listItems=[];
-  for(let i=0; i<products.length;i++){
-    const product= products[i];
-  listItems.push(
-    <li 
-    key={product.id} 
-    style={{
-      color: product.isFRUIT ? 'magenta': 'darkgreen'
-    }}
-    >
-      {product.title}
+ export default  function BusinessList(){
+const listItem= [];
+for(let i=0;i<products.length; i++){
+ const  product=products[i];
+  listItem.push(
+  <li 
+  key={
+  product.id
+  }
+  style={{
+    color:product.isFruit ? "magenta" : 'darkgreen'
+  }}
+  >
+    {product.title}
 
-    </li>
-    
-  )}
-  return(
-    <ul>
-    {listItems}
-    </ul>
-  )
+  </li>
+)
+}
+return(
+  <ul>
+    {listItem}
+  </ul>
+)
+
 }
