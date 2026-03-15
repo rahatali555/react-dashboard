4b5f32a85b303a3156c941e6566ae7a211cb02a4
rahatali555 <ali.rahat555@gmail.com>
2026-03-16 00:04:38 +0500

    learning and practicing use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
-
+import{ useState } from 'react'
 function MyButton(){
-  const[count, setCount]=useState(0);
-   
+  const[count, setCount] = useState(0);
+
   function handleClick(){
-    setCount(count+1)
+    setCount(count+1);
+
   }
   return(
     <button onClick={handleClick}>
-      Clicked {count} times
+      clicked {count} times
     </button>
   )
 }
 export default function App(){
-  return (
-    <div>
-      <h1> counters that update separetly</h1>
-      <MyButton/>
- 
-       <MyButton/> </div>
-  )
+  return(
+    <>
+    <h1> counter that updates</h1>
+    <MyButton/>
+    <MyButton/> 
+      </> );
 }
